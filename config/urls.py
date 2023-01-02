cab4fc489393426d5c97fc0536cd63241fffd210
Karthik <karthikeyan@testpress.in>
2023-01-02 19:33:29 +0530

    Create list view for question

--- config/urls.py
+++ config/urls.py
@@ -16,7 +16,7 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import include, path
 
-from question.views import dashboard
+from question.views import dashboard, question_list
 
 urlpatterns = [
     path("admin/", admin.site.urls),
@@ -24,4 +24,5 @@ urlpatterns = [
     path("__reload__/", include("django_browser_reload.urls")),
     path("account/", include("django.contrib.auth.urls")),
     path("question_hub/", dashboard.dashboard, name="dashboard"),
+    path("questions/", question_list.QuestionListView.as_view(), name="question_list"),
 ]
